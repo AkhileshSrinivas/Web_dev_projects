70c6b0e32ca2e722ee437a6a793d781b3cf7ab6d
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-17 18:34:22 +0530

    Manipulating CSS Styles

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -21,19 +21,26 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-    document.querySelector('.highscore').textContent = document.querySelector(
-      '.score'
-    ).textContent;
+    document.querySelector('.highscore').textContent = score;
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30 rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -44,6 +51,8 @@ document.querySelector('.check').addEventListener('click', function () {
       score = 0;
       document.querySelector('.score').textContent = score;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
