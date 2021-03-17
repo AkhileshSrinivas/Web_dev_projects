443cf5e7ef663f6998d8cfffce82fff12627fc2c
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-17 18:02:15 +0530

    Implementing the Game Logic

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -18,11 +18,41 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.highscore').textContent = document.querySelector(
+      '.score'
+    ).textContent;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost the game!';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost the game!';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
   }
 });
