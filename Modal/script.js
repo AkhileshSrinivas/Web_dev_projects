28151e89a0b0111fd411f86f3ff8af94a3439b33
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-18 14:37:53 +0530

    Modal Window

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btncloseModel = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModel);
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  console.log(btnsOpenModel[i].textContent);
+}
