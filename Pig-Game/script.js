92136ffdcdf51fbae0e9d97ae83b6605a404bf6a
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-18 17:19:31 +0530

    Rolling the Dice

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -4,9 +4,33 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('.current-score');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling Dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change Later
+  } else {
+    //Switch to next player
+  }
+});
