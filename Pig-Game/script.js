c3177aebdcb45233ce9a6270bd327b2a4b88b3eb
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-18 19:15:39 +0530

    Resetting the game

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -6,12 +6,13 @@ const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -65,6 +66,8 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
+      btnRoll.classList.add('hidden');
+      btnHold.classList.add('hidden');
       diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
@@ -77,4 +80,27 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+//reset
+btnNew.addEventListener('click', function () {
+  scores = [0, 0];
+  currentScore = 0;
+  score0El.textContent = currentScore;
+  score1El.textContent = currentScore;
+  current1El.textContent = currentScore;
+  current1El.textContent = currentScore;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--active');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  activePlayer = 0;
+
+  diceEl.classList.add('hidden');
+  playing = true;
+  btnRoll.classList.remove('hidden');
+  btnHold.classList.remove('hidden');
 });
