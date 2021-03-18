74f8c2d7bf306dc36a63a53530625ad779a889cf
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-18 17:39:32 +0530

    Switching the Active Player

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,16 +1,19 @@
 'use strict';
 
 // Selecting elements
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const current0El = document.querySelector('.current-score');
+const current0El = document.querySelector('#current--0');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0El.textContent = 0;
@@ -29,8 +32,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // Change Later
+    document.querySelector(
+      `#current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     //Switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
