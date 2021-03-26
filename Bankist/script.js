82d3e22900dcf51c99ddfbacc19a5c8e9dbfcc48
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-26 16:18:15 +0530

    creating usernames

--- Bankist/script.js
+++ Bankist/script.js
@@ -75,3 +75,14 @@ const displayMovemnet = function (movements) {
 };
 
 displayMovemnet(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
