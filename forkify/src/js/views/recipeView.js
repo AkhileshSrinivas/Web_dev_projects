25c85cd2e6026c6582e94b22c26c2b574cefeafa
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-04-07 17:30:33 +0530

    Event Handlers in MVC: Publisher-Subscriber Pattern

--- forkify/src/js/views/recipeView.js
+++ forkify/src/js/views/recipeView.js
@@ -32,6 +32,10 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
   #generateMarkup() {
     return `
     <figure class="recipe__fig">
@@ -118,6 +122,7 @@ class RecipeView {
     </div>
 `;
   }
+
   #generateMarkupIngredient(ing) {
     return `
       <li class="recipe__ingredient">
